178c2b62133c3b48a545c55251f20d96b9ec92b7
Charles Oppermann <chuckop@live.com>
2017-02-09 17:20:18 -0500

    Initial version of code

--- DomainInfo.cpp
+++ DomainInfo.cpp
@@ -1,11 +1,68 @@
 // DomainInfo.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "targetver.h"
+
+#include <stdio.h>
+//#include <tchar.h>
+
+#include <Windows.h>
+
+#include <LM.h>
+
+#pragma comment( lib, "Netapi32" )
+
 
 
 int main()
 {
-    return 0;
+    /*
+	//
+// Determines whether a workstation is joined to a domain or not
+//
+NET_API_STATUS
+NET_API_FUNCTION
+NetGetJoinInformation(
+    _In_opt_ LPCWSTR             lpServer,
+    _Outptr_ LPWSTR             *lpNameBuffer,
+    _Out_ PNETSETUP_JOIN_STATUS  BufferType
+    );
+	*/
+
+	wchar_t *lpszName		= NULL;		// Pointer to string which will hold domain or workgroup name
+	NETSETUP_JOIN_STATUS	joinStatus;
+
+	DWORD dwStatus = NetGetJoinInformation(NULL, &lpszName, &joinStatus);
+
+	if (NERR_Success == dwStatus)
+	{
+		switch (joinStatus)
+		{
+		case	NetSetupUnjoined:
+			wprintf(__TEXT("Machine is not joined to a workgroup or domain.\n"));
+			break;
+
+		case	NetSetupWorkgroupName:
+			wprintf(__TEXT("Machine is associated with the workgroup \"%s\"\n"), lpszName);
+			break;
+
+		case	NetSetupDomainName:
+			wprintf(__TEXT("Machine is associated with the domain \"%s\"\n"), lpszName);
+			break;
+
+		case	NetSetupUnknownStatus:
+		default:
+			wprintf(__TEXT("Call to NetGetJoinInformation succeeded, but could not determine the status.\n"));
+		};
+	}
+	else
+	{
+		wprintf(__TEXT("Call to NetGetJoinInformation failed with error code %u"), dwStatus);
+	}
+
+	if (lpszName != NULL)
+		NetApiBufferFree(lpszName);
+
+	return dwStatus;
 }
 
